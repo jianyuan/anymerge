ae76ff470d544a06425477e547c6fdf1c681fbac
Jian Yuan Lee <jianyuan@gmail.com>
2024-12-27 14:44:57 +0000

    feat: get_base_type utility

---
+++ tests/test_typing_utils.py
@@ -0,0 +1,27 @@
+import typing
+
+import pytest
+
+import anymerge._typing_utils as sut
+
+
+@pytest.mark.parametrize(
+    "annotation, expected",
+    [
+        (int, int),
+        (str, str),
+        (typing.List[int], typing.List[int]),
+        (typing.Dict[str, int], typing.Dict[str, int]),
+        (typing.Annotated[int, "metadata"], int),
+        (typing.Annotated[typing.List[int], "metadata"], typing.List[int]),
+        (typing.Annotated[typing.Union[int, str], "metadata"], [int, str]),
+        (typing.Annotated[int | str, "metadata"], [int, str]),
+        (typing.Union[int, str], [int, str]),
+        (int | str, [int, str]),
+    ],
+)
+def test_get_base_type(
+    annotation: typing.Any,
+    expected: typing.Any | typing.List[typing.Any],
+) -> None:
+    assert sut.get_base_type(annotation) == expected
