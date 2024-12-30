1917360d5ccd8314c5a326de5cfa303d23ab0f4c
Jian Yuan Lee <jianyuan@gmail.com>
2024-12-30 16:10:10 +0000

    feat: reduce

--- src/anymerge/__init__.py
+++ src/anymerge/__init__.py
@@ -1,11 +1,13 @@
 from anymerge import exceptions, models, reducers, types
 from anymerge._api import Reducer
 from anymerge._merger import merge
+from anymerge._reducer import reduce
 
 __all__ = [
     "exceptions",
     "merge",
     "models",
+    "reduce",
     "Reducer",
     "reducers",
     "types",
